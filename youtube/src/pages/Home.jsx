ee5beca5f7f94700300198a0d1bf8f711ff71d0a
1anshu-stack <71838931+1anshu-stack@users.noreply.github.com>
2024-07-07 19:10:29 +0000

    slidebar

--- youtube/src/pages/Home.jsx
+++ youtube/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "../components/Navbar";
+import Slidebar from "../components/Slidebar";
 
 function Home(){
     return (
         <>
           <Navbar />
+          <Slidebar />
         </>
     )
 }
